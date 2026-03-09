e5cc3889847850eb8e7ff500b8bc1350e7ced3a6
GGourav <67853722+GGourav@users.noreply.github.com>
2026-03-09 10:12:52 +0530

    Update MobsHandler.java

--- vpnadaptercore/src/main/java/com/minhui/vpn/Handlers/MobsHandler.java
+++ vpnadaptercore/src/main/java/com/minhui/vpn/Handlers/MobsHandler.java
@@ -17,13 +17,12 @@ public class MobsHandler {
     public class MobsInfo {
         public int tier;
         public String name;
-        public String type; // Mob, Resource, etc.
+        public String type;
     }
 
     public void loadDatabase(Context context) {
         try {
             Gson gson = new Gson();
-            // Load Mobs from JSON
             InputStreamReader reader = new InputStreamReader(context.getAssets().open("mobs.json"));
             Map<String, MobsInfo> loadedMobs = gson.fromJson(reader, new TypeToken<Map<String, MobsInfo>>(){}.getType());
             if (loadedMobs != null) mobDatabase.putAll(loadedMobs);
@@ -34,32 +33,60 @@ public class MobsHandler {
     public void AddMob(int id, int typeId, String name, float posX, float posY, int health, int enchant, int rarity) {
         SharedLocks.mobsHandlerLock.writeLock().lock();
         try {
-            String sid = String.valueOf(typeId);
             Mob m = new Mob(id, typeId, name, posX, posY, health, enchant, rarity);
-            
+            String sid = String.valueOf(typeId);
             if (mobDatabase.containsKey(sid)) {
                 MobsInfo info = mobDatabase.get(sid);
                 m.tier = info.tier;
                 m.name = info.name;
-                // Map the string type to our Enum
-                m.type = MobCodes.Enemy; 
+                m.type = MobCodes.Enemy;
                 if (info.type != null && info.type.contains("Resource")) m.type = MobCodes.Harvestable;
             }
-            
             if (!mobsList.contains(m)) mobsList.add(m);
         } finally {
             SharedLocks.mobsHandlerLock.writeLock().unlock();
         }
     }
 
-    public ArrayList<Mob> getMobList() {
-        return new ArrayList<>(mobsList);
+    // RESTORED: Required by vpnadaptercore
+    public void RemoveMob(int id) {
+        SharedLocks.mobsHandlerLock.writeLock().lock();
+        try {
+            mobsList.removeIf(m -> m.id == id);
+        } finally {
+            SharedLocks.mobsHandlerLock.writeLock().unlock();
+        }
+    }
+
+    // RESTORED: Required by vpnadaptercore
+    public void UpdateMobEnchantmentLevel(int mobId, int enchantmentLevel) {
+        SharedLocks.mobsHandlerLock.writeLock().lock();
+        try {
+            for (Mob m : mobsList) {
+                if (m.id == mobId) {
+                    m.enchantmentLevel = enchantmentLevel;
+                    break;
+                }
+            }
+        } finally {
+            SharedLocks.mobsHandlerLock.writeLock().unlock();
+        }
     }
 
     public void UpdateMobPosition(int id, float posX, float posY) {
-        for (Mob m : mobsList) {
-            if (m.id == id) { m.posX = posX; m.posY = posY; break; }
+        SharedLocks.mobsHandlerLock.writeLock().lock();
+        try {
+            for (Mob m : mobsList) {
+                if (m.id == id) { m.posX = posX; m.posY = posY; break; }
+            }
+        } finally {
+            SharedLocks.mobsHandlerLock.writeLock().unlock();
         }
+    }
+
+    public ArrayList<Mob> getMobList() {
+        SharedLocks.mobsHandlerLock.readLock().lock();
+        try { return new ArrayList<>(mobsList); } finally { SharedLocks.mobsHandlerLock.readLock().unlock(); }
     }
 
     public void clear() { mobsList.clear(); }
