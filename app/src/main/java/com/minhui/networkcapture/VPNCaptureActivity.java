2a79b9a44d32040fd2ba909bde71f2c9c42d80b1
GGourav <67853722+GGourav@users.noreply.github.com>
2026-03-09 14:16:45 +0530

    Update VPNCaptureActivity.java

--- app/src/main/java/com/minhui/networkcapture/VPNCaptureActivity.java
+++ app/src/main/java/com/minhui/networkcapture/VPNCaptureActivity.java
@@ -1,55 +1,36 @@
 package com.minhui.networkcapture;
 
-import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.provider.Settings;
 import android.widget.ImageView;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Keep;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 import com.minhui.networkcapture.Fragments.MainFragment;
-import com.minhui.networkcapture.RadarView.RadarSettings;
-import com.minhui.vpn.ProxyConfig;
 import com.minhui.vpn.utils.VpnServiceHelper;
 import com.minhui.vpn.Handlers.MainHandler;
 
 @Keep
 public class VPNCaptureActivity extends FragmentActivity {
     private ImageView vpnButton;
-    private Handler handler;
-    Intent windowService;
-
-    ProxyConfig.VpnStatusListener vpnStatusListener = new ProxyConfig.VpnStatusListener() {
-        @Override
-        public void onVpnStart(Context context) {
-            handler.post(()->vpnButton.setImageResource(R.mipmap.ic_stop));
-            startRadarWindowService();
-        }
-        @Override
-        public void onVpnEnd(Context context) {
-            handler.post(()->vpnButton.setImageResource(R.mipmap.ic_start));
-            if(windowService!=null) stopService(windowService);
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vpn_capture);
-        handler = new Handler();
         MainHandler.getInstance().initDatabase(this);
 
         vpnButton = findViewById(R.id.vpn);
         vpnButton.setOnClickListener(v -> {
-          if(VpnServiceHelper.vpnRunningStatus()) VpnServiceHelper.changeVpnRunningStatus(this,false);
-          else VpnServiceHelper.changeVpnRunningStatus(this,true);
+            boolean isRunning = VpnServiceHelper.vpnRunningStatus();
+            VpnServiceHelper.changeVpnRunningStatus(this, !isRunning);
+            
+            if (!isRunning) {
+                // Delay showing window slightly so VPN can establish
+                vpnButton.postDelayed(this::startRadarWindowService, 1000);
+            }
         });
 
         if (!Settings.canDrawOverlays(this)) {
@@ -57,18 +38,14 @@ public class VPNCaptureActivity extends FragmentActivity {
         }
 
         getSupportFragmentManager().beginTransaction().replace(R.id.container_vp, new MainFragment()).commit();
-        RadarSettings.getInstance().init(getApplicationContext());
-        ProxyConfig.Instance.registerVpnStatusListener(vpnStatusListener);
     }
 
     private void startRadarWindowService() {
-        windowService = new Intent(this, RadarDrawView.class);
-        ContextCompat.startForegroundService(this, windowService);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        ProxyConfig.Instance.unregisterVpnStatusListener(vpnStatusListener);
+        Intent intent = new Intent(this, RadarDrawView.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            ContextCompat.startForegroundService(this, intent);
+        } else {
+            startService(intent);
+        }
     }
 }
