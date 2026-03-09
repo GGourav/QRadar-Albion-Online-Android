91f91b92e2b29f6304164a257bae00ffd6b2a722
GGourav <67853722+GGourav@users.noreply.github.com>
2026-03-09 12:16:46 +0530

    Update VPNCaptureActivity.java

--- app/src/main/java/com/minhui/networkcapture/VPNCaptureActivity.java
+++ app/src/main/java/com/minhui/networkcapture/VPNCaptureActivity.java
@@ -1,7 +1,5 @@
 package com.minhui.networkcapture;
 
-import static com.minhui.vpn.utils.VpnServiceHelper.START_VPN_SERVICE_REQUEST_CODE;
-
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
@@ -10,164 +8,67 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings;
 import android.widget.ImageView;
-import android.widget.TextView;
-
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Keep;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
-
 import com.minhui.networkcapture.Fragments.MainFragment;
 import com.minhui.networkcapture.RadarView.RadarSettings;
 import com.minhui.vpn.ProxyConfig;
-import com.minhui.vpn.utils.DebugLog;
 import com.minhui.vpn.utils.VpnServiceHelper;
 import com.minhui.vpn.Handlers.MainHandler;
 
 @Keep
-public class VPNCaptureActivity extends FragmentActivity
-{
-    private static final int REQUEST_STORAGE_PERMISSION = 104;
-    private static final int REQUEST_OVERLAY_PERMISSION = 5469;
-    public static Context mContext;
+public class VPNCaptureActivity extends FragmentActivity {
+    private ImageView vpnButton;
+    private Handler handler;
+    Intent windowService;
 
-    ProxyConfig.VpnStatusListener vpnStatusListener = new ProxyConfig.VpnStatusListener()
-    {
+    ProxyConfig.VpnStatusListener vpnStatusListener = new ProxyConfig.VpnStatusListener() {
         @Override
-        public void onVpnStart(Context context)
-        {
+        public void onVpnStart(Context context) {
             handler.post(()->vpnButton.setImageResource(R.mipmap.ic_stop));
             startRadarWindowService();
         }
-
         @Override
-        public void onVpnEnd(Context context)
-        {
+        public void onVpnEnd(Context context) {
             handler.post(()->vpnButton.setImageResource(R.mipmap.ic_start));
-
-            if(windowService!=null)
-            {
-                stopService(windowService);
-            }
+            if(windowService!=null) stopService(windowService);
         }
     };
 
-    private ImageView vpnButton;
-    String[] needPermissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
-    private Handler handler;
-
     @Override
-    public void onBackPressed()
-    {
-        try {
-            finishAffinity();
-            if(windowService != null) stopService(windowService);
-            System.exit(0);
-        } catch (Exception ex) {}
-    }
-
-    ActivityResultLauncher<Intent> overlayPermissionResultLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            result -> {
-                if (!Settings.canDrawOverlays(this)) {
-                    // Handle permission denied
-                }
-            }
-    );
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vpn_capture);
+        handler = new Handler();
+        MainHandler.getInstance().initDatabase(this);
 
-        mContext = this;
-
-        try {
-            MainHandler.getInstance().initDatabase(this);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.toolbar));
-        vpnButton = (ImageView) findViewById(R.id.vpn);
-
+        vpnButton = findViewById(R.id.vpn);
         vpnButton.setOnClickListener(v -> {
-          if(VpnServiceHelper.vpnRunningStatus()) closeVpn();
-          else startVPN();
+          if(VpnServiceHelper.vpnRunningStatus()) VpnServiceHelper.changeVpnRunningStatus(this,false);
+          else VpnServiceHelper.changeVpnRunningStatus(this,true);
         });
 
-        initMainFragment();
-        requestStoragePermission();
-
-        vpnButton.setEnabled(true);
-        ProxyConfig.Instance.registerVpnStatusListener(vpnStatusListener);
-
         if (!Settings.canDrawOverlays(this)) {
-            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:" + getPackageName()));
-            overlayPermissionResultLauncher.launch(intent);
+            startActivity(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName())));
         }
 
-        MainFragment newFragment = new MainFragment();
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.container_vp, newFragment)
-                .addToBackStack(null)
-                .commit();
-
-        handler = new Handler();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container_vp, new MainFragment()).commit();
         RadarSettings.getInstance().init(getApplicationContext());
+        ProxyConfig.Instance.registerVpnStatusListener(vpnStatusListener);
     }
 
-    Intent windowService;
-
-    public void initMainFragment() {}
-
-    @Override
-    protected void onStop() { super.onStop(); }
-
-    private void startRadarWindowService()
-    {
-        DebugLog.i("startRadar(%d)\n", 1);
+    private void startRadarWindowService() {
         windowService = new Intent(this, RadarDrawView.class);
-        startService(windowService);
-    }
-
-    // FIX: Changed 'fun' to 'void' (Java syntax)
-    private void requestStoragePermission()
-    {
-        ActivityCompat.requestPermissions(this, needPermissions, REQUEST_STORAGE_PERMISSION);
-    }
-
-    private void closeVpn() {
-        VpnServiceHelper.changeVpnRunningStatus(this,false);
-    }
-
-    private void startVPN() {
-        VpnServiceHelper.changeVpnRunningStatus(this,true);
+        ContextCompat.startForegroundService(this, windowService);
     }
 
     @Override
-    protected void onDestroy()
-    {
-        super.onDestroy() ;
+    protected void onDestroy() {
+        super.onDestroy();
         ProxyConfig.Instance.unregisterVpnStatusListener(vpnStatusListener);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == REQUEST_OVERLAY_PERMISSION) {
-            if (Settings.canDrawOverlays(this)) startRadarWindowService();
-        }
-
-        if (requestCode == START_VPN_SERVICE_REQUEST_CODE && resultCode == RESULT_OK) {
-            VpnServiceHelper.startVpnService(getApplicationContext());
-        }
     }
 }
